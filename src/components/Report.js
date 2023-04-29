c50988b177909b994229f0fe84565e84da6eedd0
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-29 20:06:10 -0300

    improve chart style

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
-
+import { ChartWrapper } from "../style/styles";
 export default function Report({ data }) {
   if (!data) {
     return (
@@ -15,10 +15,10 @@ export default function Report({ data }) {
 
   if (isString) {
     return (
-      <DataContainer>
+      <ReportWrapper>
         <h2>Dados IDF</h2>
         <Message>{data}</Message>
-      </DataContainer>
+      </ReportWrapper>
     );
   }
 
@@ -34,23 +34,25 @@ export default function Report({ data }) {
   const chartData = processDataForChart(data);
 
   return (
-    <DataContainer>
+    <ReportWrapper>
       <h2>Dados IDF</h2>
-      <LineChart
-        width={600}
-        height={300}
-        data={chartData}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="Tempo de Retorno (anos)" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="i Real" stroke="#82ca9d" />
-        <Line type="monotone" dataKey="i Calculado" stroke="#ff7300" />
-      </LineChart>
+      <ChartWrapper>
+        <LineChart
+          width={600}
+          height={300}
+          data={chartData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="Tempo de Retorno (anos)" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="i Real" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="i Calculado" stroke="#ff7300" />
+        </LineChart>
+      </ChartWrapper>
       <Table>
         <thead>
           <tr>
@@ -78,7 +80,7 @@ export default function Report({ data }) {
           })}
         </tbody>
       </Table>
-    </DataContainer>
+    </ReportWrapper>
   );
 }
 
@@ -95,7 +97,7 @@ const Message = styled.p`
   color: #333;
 `;
 
-const DataContainer = styled.div`
+const ReportWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -132,111 +134,3 @@ const Table = styled.table`
   }
 `;
 
-
-
-// import React from "react";
-// import styled from "styled-components";
-
-// export default function Report({ data }) {
-//   if (!data) {
-//     return (
-//       <MessageContainer>
-//         <Message>Nenhum dado disponível. Faça o upload de um arquivo CSV.</Message>
-//       </MessageContainer>
-//     );
-//   }
-
-//   const isString = typeof data === "string";
-
-//   if (isString) {
-//     return (
-//       <DataContainer>
-//         <h2>Dados IDF</h2>
-//         <Message>{data}</Message>
-//       </DataContainer>
-//     );
-//   }
-
-//   return (
-//     <DataContainer>
-//       <h2>Dados IDF</h2>
-//       <Table>
-//         <thead>
-//           <tr>
-//             <th>Índice</th>
-//             <th>Tempo de Retorno (anos)</th>
-//             <th>Coeficiente de Agregação</th>
-//             <th>i Real</th>
-//             <th>i Calculado</th>
-//             <th>Erro Relativo</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {Object.keys(data).map((key) => {
-//             const rowData = data[key];
-//             return (
-//               <tr key={key}>
-//                 <td>{key}</td>
-//                 <td>{rowData["Tr (anos)"]}</td>
-//                 <td>{rowData["td (min)"]}</td>
-//                 <td>{rowData["i_real"]}</td>
-//                 <td>{rowData["i_calculado"]}</td>
-//                 <td>{rowData["erro_relativo"]}</td>
-//               </tr>
-//             );
-//           })}
-//         </tbody>
-//       </Table>
-//     </DataContainer>
-//   );
-// }
-
-// const MessageContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   height: 200px;
-//   margin-top: 40px;
-// `;
-
-// const Message = styled.p`
-//   font-size: 18px;
-//   color: #333;
-// `;
-
-// const DataContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   width: 100%;
-//   margin-top: 40px;
-//   background-color: #ffffff;
-//   border-radius: 8px;
-//   padding: 20px;
-//   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-// `;
-
-// const Table = styled.table`
-//   border-collapse: collapse;
-//   width: 100%;
-//   text-align: center;
-//   margin-top: 20px;
-
-//   th,
-//   td {
-//     border: 1px solid #ccc;
-//     padding: 10px;
-//   }
-
-//   th {
-//     background-color: #f2f2f2;
-//   }
-
-//   tbody tr:nth-child(odd) {
-//     background-color: #f8f8f8;
-//   }
-
-//   tbody tr:hover {
-//     background-color: #ddd;
-//   }
-// `;
